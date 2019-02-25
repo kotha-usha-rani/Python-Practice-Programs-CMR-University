9b47c0952cd8d4f3c0f8b04998b859379bc224b4
kothausha <47881212+kothausha@users.noreply.github.com>
2019-02-25 10:19:15 +0530

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,26 @@
+import random
+l=["r","p","s"]
+while True:
+	u=input("enter your choice,enter e to exit")
+	if u=="e":
+		print("game over")
+		exit()
+	c=random.choice(l)
+	print("computer chooses",c)
+	if u==c:
+		print("tie")
+	elif u=="r" and c=="p":
+		print("u wins!")
+	elif u=="p" and c=="s":
+		print("c wins!")
+	elif u=="s" and c=="r":
+		print("c wins!")
+	elif u=="r" and c=="s":
+		print("u wins!")
+	elif u=="p" and c=="r":	
+		print("c wins!")
+	elif u=="s" and c=="p":	
+		print("u wins!")
+	else:
+		print("wrong choice")
+	
